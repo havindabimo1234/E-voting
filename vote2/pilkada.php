9202a31e09d567d16ab5bdb1a222e04f897c00e7
havindabimo1234 <41879991+havindabimo1234@users.noreply.github.com>
2019-04-27 11:41:15 +0700

    Add files via upload

---
+++ vote2/pilkada.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+    <h1>Tata Cara Pemilihan Kepala Daerah</h1>
+</div>
+<p>Hasil hitungan real count KPU menunjukkan pasangan Jokowi-Ma'ruf
+masih unggul tipis sementara dalam perolehan suara.
+Dari data masuk yang sudah mencapai 26,45 persen (7.688 TPS) pada Rabu pagi (24/4/2019),
+pasangan calon nomor 01 mendulang suara 879.602 (52,67 persen).
+Sedangkan pasangan 02 meraih 790.396 suara (47,33 persen).
+Dari enam wilayah Ibu Kota, Jokowi-Ma'ruf menguasai empat wilayah,
+yakni Jakarta Barat, Jakarta Pusat, Jakarta Utara dan Kepulauan Seribu. Sedangkan Prabowo-Sandi menang di Jakarta Selatan dan Jakarta Timur.</P>
